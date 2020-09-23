2c81a8fd156e01b7889853b5d94c822720ded8a8
Sergei Velichkin <sergey.velichkin@gmail.com>
2020-09-23 12:08:21 -0600

    adding payment functionality using stripe

---
+++ src/Payment.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import ShoppingCartItem from "./ShoppingCartItem";
+import { Link } from "@material-ui/core";
+
+function Payment() {
+  const [{ basket, user }] = useStateValue();
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout"> {basket?.length} items </Link>)
+        </h1>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React ave</p>
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <ShoppingCartItem
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                rating={item.rating}
+                price={item.price}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,10 +3,11 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
   const [{ basket }] = useStateValue();
-
+  const history = useHistory();
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -26,7 +27,12 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"$"}
       />
-      <button className="subtotal__button">Proceed to checkout</button>
+      <button
+        onClick={(e) => history.push("/payment")}
+        className="subtotal__button"
+      >
+        Proceed to checkout
+      </button>
     </div>
   );
 }
